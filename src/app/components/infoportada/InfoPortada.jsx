6c65fd421b887f58f125ae08156c82a8e59d0bf5
Aldo <aldo.choque@tecsup.edu.pe>
2024-04-03 21:33:06 -0500

    whatsap

--- src/app/components/infoportada/InfoPortada.jsx
+++ src/app/components/infoportada/InfoPortada.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const TargetDiv = styled.div`
@@ -17,19 +18,37 @@ const BtDiv = styled.div`
     cursor: pointer;
 `;
 
+const Popup = styled.div`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: white;
+    padding: 20px;
+    border-radius: 5px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+`;
+
 import { IoLocationSharp } from "react-icons/io5";
 import { FaWhatsapp, FaTiktok, FaSquareFacebook } from "react-icons/fa6";
 import { GiTakeMyMoney } from "react-icons/gi";
 
+
 const InfoPortada = () => {
+    const [showPopup, setShowPopup] = useState(false);
+
     const xy = [
         { val: "Ofertas", icon: <GiTakeMyMoney style={{ fontSize: "25px" }} /> },
         { val: "Whatsapp", icon: <FaWhatsapp style={{ fontSize: "25px" }} />, link: "https://wa.me/51944202238" },
         { val: "Ubicacion", icon: <IoLocationSharp style={{ fontSize: "25px" }} />, link: "https://www.google.com/maps/@-16.4215725,-71.5083146,20z?hl=es&entry=ttu" }
     ];
 
-    const handleClick = (link) => {
-        window.open(link, '_blank');
+    const handleClick = (val) => {
+        if (val === "Ofertas") {
+            setShowPopup(true);
+        } else {
+            window.open(val.link, '_blank');
+        }
     };
 
     return (
@@ -40,12 +59,17 @@ const InfoPortada = () => {
                         <BtDiv
                             key={index}
                             isUbicacion={x.val === "Ubicacion"}
-                            onClick={() => handleClick(x.link)}
+                            onClick={() => handleClick(x.val)}
                         >
                             {x.icon}{x.val}
                         </BtDiv>
                     ))}
                 </TargetDiv>
+                {showPopup && (
+                    <Popup onClick={() => setShowPopup(false)}>
+                        Por el momento no hay ofertas disponibles.
+                    </Popup>
+                )}
             </div>
         </div>
     );
