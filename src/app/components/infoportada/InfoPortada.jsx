5f229a502bbe1ac54f2d3117c9e32a69b64588e2
Aldo <aldo.choque@tecsup.edu.pe>
2024-06-17 09:38:38 -0500

    220

--- src/app/components/infoportada/InfoPortada.jsx
+++ src/app/components/infoportada/InfoPortada.jsx
@@ -74,7 +74,7 @@ const InfoPortada = () => {
                 </TargetDiv>
                 {showPopup && (
                     <Popup onClick={() => setShowPopup(false)}>
-                        Examen medico en oferta a solo S/.80 aprovecha esta OFERTA 
+                        Examen medico en oferta a solo S/.220 aprovecha esta OFERTA 
                     </Popup>
                 )}
             </div>
